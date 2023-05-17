f92ef5a5fb185a54e42404a76371253a6393fa43
Ruamez <ruamepnn@gmail.com>
2023-05-17 10:01:25 +0200

    17/5/23 Fixed entity: StoricoCommesse
    im dumb as a rock :D

--- Sincrono/src/main/java/it/sincrono/entities/StoricoCommesse.java
+++ Sincrono/src/main/java/it/sincrono/entities/StoricoCommesse.java
@@ -1,12 +1,17 @@
 package it.sincrono.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+
+@Entity
+@Table(name = "storico_commesse")
 public class StoricoCommesse {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
