35b879475e50881f74f8ce72ca6e814a3ff5c9bc
davidesaltarelli00 <133639028+davidesaltarelli00@users.noreply.github.com>
2023-08-29 16:31:15 +0200

    29082023_email_service

--- Sincrono/src/main/java/it/sincrono/services/impl/AnagraficaServiceImpl.java
+++ Sincrono/src/main/java/it/sincrono/services/impl/AnagraficaServiceImpl.java
@@ -290,10 +290,10 @@ public class AnagraficaServiceImpl extends BaseServiceImpl implements Anagrafica
 						.saveAndFlush(new StoricoContratti(new Anagrafica(idAnagrafica), new Contratto(idContratto)));
 			}
 
-//			emailService.sendMail(null, anagraficaDto.getAnagrafica().getMailAziendale(), null,"CREAZIONE UTENZA","username:"+
-//					anagraficaDto.getAnagrafica().getUtente().getUsername()+"\n"+
-//					"password:"+passwordUtente);
-//			
+			emailService.sendMail(null, anagraficaDto.getAnagrafica().getMailAziendale(), null,"CREAZIONE UTENZA","username:"+
+					anagraficaDto.getAnagrafica().getUtente().getUsername()+"\n"+
+					"password:"+passwordUtente);
+			
 			System.out.println("Password" + passwordUtente);
 
 		} catch (DataIntegrityViolationException de) {
