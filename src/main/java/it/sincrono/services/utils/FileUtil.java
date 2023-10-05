c713a8ee7149216813d5a8686dbbd882cf605e6c
aandreii02 <121028501+Andrei-Musat-Cosmin@users.noreply.github.com>
2023-10-05 17:25:22 +0200

    05102023_file

--- src/main/java/it/sincrono/services/utils/FileUtil.java
+++ src/main/java/it/sincrono/services/utils/FileUtil.java
@@ -16,7 +16,7 @@ import java.util.List;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.TypeMismatchException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
 
 import it.sincrono.repositories.dto.GiornoDto;
@@ -156,7 +156,7 @@ public class FileUtil {
 								giorno.getGiorno() + "," + giorno.getCliente() + "," + giorno.getOreOrdinarie() + ";");
 					}
 				}
-			} catch (TypeMismatchException e) {
+			} catch (HttpMessageNotReadableException e) {
 				LOGGER.log(Level.ERROR, e.getMessage());
 				throw new ServiceException(ServiceMessages.FORMATO_INCORRETTO);
 			} catch (IOException e) {
