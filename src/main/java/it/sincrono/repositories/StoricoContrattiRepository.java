d278de2d16cefb730790676a6dbff37db5c636c4
davidesaltarelli00 <133639028+davidesaltarelli00@users.noreply.github.com>
2023-10-27 16:35:05 +0200

    storicoContrattiResolve

--- src/main/java/it/sincrono/repositories/StoricoContrattiRepository.java
+++ src/main/java/it/sincrono/repositories/StoricoContrattiRepository.java
@@ -1,12 +1,22 @@
 package it.sincrono.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import it.sincrono.entities.Contratto;
 import it.sincrono.entities.StoricoContratti;
 
 @Repository
 public interface StoricoContrattiRepository
 		extends JpaRepository<StoricoContratti, Integer>, StoricoContrattiCustomRepository {
+	
+	
+	@Query("select sc.contratto from StoricoContratti sc where sc.anagrafica.id=(?1) and sc.contratto.attivo=false")
+	List<Contratto> getStoricoContratti(Integer idAnagrafica);
+	
+	
 
 }
