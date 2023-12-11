cd9749fbdaaffccbb3896d7565cb51d85cf7d4df
davidesaltarelli <86360300+davidesaltarelli@users.noreply.github.com>
2023-12-11 15:52:54 +0100

    updateRichiesta

--- src/main/java/it/sincrono/repositories/TipoRichiestaRepository.java
+++ src/main/java/it/sincrono/repositories/TipoRichiestaRepository.java
@@ -22,7 +22,7 @@ public interface TipoRichiestaRepository extends JpaRepository<TipoRichieste, In
 	@Query("SELECT a FROM TipoRichieste a WHERE a.richiesta.id=?1")
 	public List<TipoRichieste> getTipoRichiesteByIdRichieste(Integer idRichiesta);
 	
-	@Query("select tr from tipoRichieste tr where tr.richieste.anno=?1 and tr.richieste.mese=?2 and tr.anagrafica.id=?3 and tr.nGiorno=?4 and ((TIME(?5) <= TIME(tr.a) AND TIME(?6) >= TIME(tr.da)) OR (TIME(?5)) >= TIME(tr.da) AND TIME(?6) <= TIME(tr.a)))")
+	@Query("select tr from TipoRichieste tr where tr.richiesta.anno=?1 and tr.richiesta.mese=?2 and tr.richiesta.anagrafica.id=?3 and tr.nGiorno=?4 and ((TIME(?5) <= TIME(tr.aOra) AND TIME(?6) >= TIME(tr.daOra)) OR (TIME(?5)) >= TIME(tr.daOra) AND TIME(?6) <= TIME(tr.aOra))")
 	public List<TipoRichieste> getRichiesteValidate(Integer anno,Integer mese,Integer idAnagrafica,Integer ngiorno,String aOra,String daOra);
 
 
